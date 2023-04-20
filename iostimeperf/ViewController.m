148916cf0cfbf43f07399d01853ac08308bf2c28
larryhou <larryhou@tencent.com>
2023-04-20 15:36:58 +0800

    Update

--- iostimeperf/ViewController.m
+++ iostimeperf/ViewController.m
@@ -14,10 +14,10 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
+- (void)viewDidAppear: (BOOL) animated {
+    [super viewDidAppear: animated];
     runtest();
-}
+};
 
 
 @end
--- test/test.hpp
+++ test/test.hpp
@@ -12,7 +12,7 @@
 extern "C" {
 #endif
 
-void runtest();
+void runtest(void);
 #ifdef __cplusplus
 }
 #endif
